d4d737633eed78f5ae84b092542346638d58a279
Alessandro <me@alexdevs.me>
2021-10-04 22:48:13 +0200

    Now using cryptographically secure RNG

--- DevBin/Utils.cs
+++ DevBin/Utils.cs
@@ -3,26 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DevBin
 {
     public static class Utils
     {
+        private static RNGCryptoServiceProvider _random = new();
         public static string RandomAlphaString(int length = 8)
         {
-            Random rng = new();
+            byte[] numbers = new byte[length];
+            _random.GetBytes(numbers);
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            int i = 0;
             return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[rng.Next(s.Length)]).ToArray());
+                .Select(s => {
+                    int j = numbers[i] % chars.Length;
+                    i++;
+                    return s[j];
+                }).ToArray());
         }
 
         public static string RandomString(int length = 16)
         {
-            Random rng = new();
+            byte[] numbers = new byte[length];
+            _random.GetBytes(numbers);
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.";
+            int i = 0;
             return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[rng.Next(s.Length)]).ToArray());
+                .Select(s => {
+                    int j = numbers[i] % chars.Length;
+                    i++;
+                    return s[j];
+                }).ToArray());
         }
 
         public static bool ValidatePassword(User user, string password)
