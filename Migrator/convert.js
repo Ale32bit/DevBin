1c94a52c400a796152c8b91270a95d543acd921a
Alessandro Proto <me@alexdevs.me>
2022-04-17 12:56:38 +0200

    Made scripts for migrating database

---
+++ Migrator/convert.js
@@ -0,0 +1,75 @@
+const fs = require("fs")
+const mysql = require("mysql");
+
+function dateToSqlFormat(d) {
+    return d.toISOString().slice(0, 19).replace('T', ' ');
+}
+
+function decodeToUTF8(str) {
+    return decodeURIComponent(str.replace(/^0x/, '').replace(/[0-9a-f]{2}/g, '%$&'));
+}
+
+function passwordToString(password) {
+    return String.fromCharCode(...password.data);
+}
+
+console.log("Migrating users and API Tokens...")
+const users = require("./input/users.json")
+
+let usersQuery = "INSERT INTO AspNetUsers (Id, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount, LegacyPassword) VALUES ";
+let tokensQuery = "INSERT INTO ApiTokens (Name, Token, CreatedAt, OwnerId, AllowGet, AllowCreate, AllowUpdate, AllowDelete, AllowGetUser, AllowCreateFolders, AllowDeleteFolders) VALUES ";
+
+for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    usersQuery += `\n( ${user.id}, ${mysql.escape(user.username)}, ${mysql.escape(user.username.toUpperCase())}, ${mysql.escape(user.email)}, ${mysql.escape(user.email.toUpperCase())}, ${user.verified}, 0, 0, 1, 0, '${passwordToString(user.password).replace(/\u0000/g, "0")}' ),`;
+    if (user.apiToken !== null && user.apiToken != undefined) {
+        tokensQuery += `\n('Migrated from v2', '${user.apiToken}', '${dateToSqlFormat(new Date())}', ${user.id}, 1, 1, 1, 1, 1, 0, 0),`;
+    }
+}
+
+fs.writeFileSync("./output/users.sql", usersQuery.substring(0, usersQuery.length - 1) + ";");
+fs.writeFileSync("./output/tokens.sql", tokensQuery.substring(0, tokensQuery.length - 1) + ";");
+
+console.log("Migrating syntaxes...");
+const syntaxes = require("./input/syntaxes.json");
+
+let syntaxesQuery = "INSERT INTO Syntaxes (Name, DisplayName, IsHidden) VALUES ";
+
+let rSyntax = [];
+
+for (let i = 0; i < syntaxes.length; i++) {
+    let syntax = syntaxes[i];
+    syntaxesQuery += `\n('${syntax.name}', '${syntax.pretty}', ${syntax.show == "1" ? 0 : 1}),`;
+    rSyntax.push(syntax.name);
+}
+fs.writeFileSync("./output/syntaxes.sql", syntaxesQuery.substring(0, syntaxesQuery.length - 1) + ";");
+
+let exposuresQuery = `INSERT INTO Exposures (Id, Name, IsListed, IsAuthorOnly) VALUES
+(1, 'Public', 1, 0),
+(2, 'Unlisted', 0, 0),
+(3, 'Private', 0, 1);`;
+fs.writeFileSync("./output/exposures.sql", exposuresQuery);
+
+console.log("Migrating pastes...");
+const pastes = require("./input/pastes.json");
+
+let pastesQuery = "INSERT INTO Pastes (Code, Title, Views, DateTime, UpdateDateTime, Cache, Content, UploaderIPAddress, SyntaxName, ExposureId, AuthorId) VALUES ";
+for (let i = 0; i < pastes.length; i++) {
+    let paste = pastes[i];
+    if (paste.exposureId == "4") paste.exposureId = "2";
+    if (paste.authorId == "0") paste.authorId = null;
+
+    pastesQuery += `\n('${paste.code}', ${mysql.escape(paste.title)}, ${paste.views}, '${paste.datetime}', ${paste.updateDatetime != null ? `'${paste.updateDatetime}'` : 'NULL'}, ${mysql.escape(paste.cache)}, ${mysql.escape(paste.content)}, ${paste.ipAddress != null ? `'${paste.ipAddress}'` : "'0.0.0.0'"}, '${rSyntax[parseInt(paste.syntaxId)]}', ${paste.exposureId}, ${paste.authorId}),`;
+}
+
+fs.writeFileSync("./output/pastes.sql", pastesQuery.substring(0, pastesQuery.length - 1) + ";");
+
+let fullOutput = usersQuery.substring(0, usersQuery.length - 1) + ";\n"
+    + tokensQuery.substring(0, tokensQuery.length - 1) + ";\n"
+    + syntaxesQuery.substring(0, syntaxesQuery.length - 1) + ";\n"
+    + exposuresQuery + "\n"
+    + pastesQuery.substring(0, pastesQuery.length - 1) + ";\n";
+fs.writeFileSync("./output/full.sql", fullOutput);
+
+console.log("Migrated all data to the new format!")
+console.log("Import the output/full.sql file to the new database!")
---
+++ Migrator/extractData.js
@@ -0,0 +1,54 @@
+const mariadb = require('mariadb');
+const fs = require('fs');
+const pool = mariadb.createPool(require("./dbConf.json"));
+async function query(...pars) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        return await conn.query(...pars);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
+async function main() {
+    if(!fs.existsSync("./output")){
+        fs.mkdirSync("./output");
+    }
+
+    if(!fs.existsSync("./input")){
+        fs.mkdirSync("./input");
+    }
+
+    console.log("Extracting users...");
+    let users = await query("SELECT * FROM users");
+    let usersOutput = [];
+    for (let i = 0; i < users.length; i++) {
+        usersOutput.push(users[i]);
+    }
+    fs.writeFileSync("./input/users.json", JSON.stringify(usersOutput));
+
+    console.log("Extracting syntaxes...");
+    let syntaxes = await query("SELECT * FROM syntaxes");
+    let syntaxesOutput = [];
+    for (let i = 0; i < syntaxes.length; i++) {
+        syntaxesOutput.push(syntaxes[i]);
+    }
+    fs.writeFileSync("./input/syntaxes.json", JSON.stringify(syntaxesOutput));
+
+    console.log("Extracting pastes...");
+    let pastesOutput = [];
+    let pastes = await query("SELECT * FROM pastes");
+    for (let i = 0; i < pastes.length; i++) {
+        pastesOutput.push(pastes[i]);
+    }
+    fs.writeFileSync("./input/pastes.json", JSON.stringify(pastesOutput));
+
+
+    console.log("Run convert.js to migrate to the new format.")
+    process.exit(0)
+}
+
+main();
