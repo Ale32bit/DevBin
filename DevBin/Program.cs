187774a887947c2ae4b4c5cb4d12dea4694da103
Alessandro Proto <me@alexdevs.me>
2022-08-01 23:44:42 +0200

    UI tweaks, error page showing for status codes 4xx and 5xx

--- DevBin/Program.cs
+++ DevBin/Program.cs
@@ -215,9 +215,13 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStatusCodePages();
+app.UseStatusCodePagesWithReExecute("/Error");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
